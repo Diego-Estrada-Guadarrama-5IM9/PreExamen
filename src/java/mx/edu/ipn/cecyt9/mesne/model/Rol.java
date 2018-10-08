228b2d235f05c0db2114d2a518923f60d51af5e7
Alumno <Alumno@LDS-32>
2018-10-08 08:51:37 -0500

    Preexamen

---
+++ src/java/mx/edu/ipn/cecyt9/mesne/model/Rol.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.edu.ipn.cecyt9.mesne.model;
+
+import java.io.Serializable;
+
+
+/**
+ *
+ * @author Alumno
+ */
+
+public class Rol implements Serializable {
+
+
+    private Integer idrol;
+
+    private String descripcion;
+
+    public Rol() {
+    }
+
+    public Rol(Integer idrol) {
+        this.idrol = idrol;
+    }
+
+    public Integer getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(Integer idrol) {
+        this.idrol = idrol;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idrol != null ? idrol.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Rol)) {
+            return false;
+        }
+        Rol other = (Rol) object;
+        if ((this.idrol == null && other.idrol != null) || (this.idrol != null && !this.idrol.equals(other.idrol))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "mx.edu.ipn.cecyt9.mesne.model.Rol[ idrol=" + idrol + " ]";
+    }
+    
+}
